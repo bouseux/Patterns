1b895cc83562052ebec2ba2d18454ac4709e4c32
Yiorgis Gozadinos <ggozad@crypho.com>
2014-02-10 13:07:22 +0100

    Omit defineImagesLoaded and do it properly

--- src/extras/imagesloaded.js
+++ src/extras/imagesloaded.js
@@ -15,47 +15,45 @@
 
 }(this, function ($, EventEmitter, eventie) {
 
-'use strict';
 
-var console = window.console;
-var hasConsole = typeof console !== 'undefined';
+  'use strict';
+  var console = window.console;
 
-// -------------------------- helpers -------------------------- //
+  var hasConsole = typeof console !== 'undefined';
+  // -------------------------- helpers -------------------------- //
 
-// extend objects
-function extend( a, b ) {
-  for ( var prop in b ) {
-    a[ prop ] = b[ prop ];
-  }
-  return a;
-}
-
-var objToString = Object.prototype.toString;
-function isArray( obj ) {
-  return objToString.call( obj ) === '[object Array]';
-}
-
-// turn element or nodeList into an array
-function makeArray( obj ) {
-  var ary = [];
-  if ( isArray( obj ) ) {
-    // use object if already an array
-    ary = obj;
-  } else if ( typeof obj.length === 'number' ) {
-    // convert nodeList to array
-    for ( var i=0, len = obj.length; i < len; i++ ) {
-      ary.push( obj[i] );
+  // extend objects
+  function extend( a, b ) {
+    for ( var prop in b ) {
+      a[ prop ] = b[ prop ];
     }
-  } else {
-    // array of single index
-    ary.push( obj );
+    return a;
   }
-  return ary;
-}
 
-// --------------------------  -------------------------- //
+  var objToString = Object.prototype.toString;
+  function isArray( obj ) {
+    return objToString.call( obj ) === '[object Array]';
+  }
 
-function defineImagesLoaded( EventEmitter, eventie ) {
+  // turn element or nodeList into an array
+  function makeArray( obj ) {
+    var ary = [];
+    if ( isArray( obj ) ) {
+      // use object if already an array
+      ary = obj;
+    } else if ( typeof obj.length === 'number' ) {
+      // convert nodeList to array
+      for ( var i=0, len = obj.length; i < len; i++ ) {
+        ary.push( obj[i] );
+      }
+    } else {
+      // array of single index
+      ary.push( obj );
+    }
+    return ary;
+  }
+
+  // --------------------------  -------------------------- //
 
   /**
    * @param {Array, Element, NodeList, String} elem
@@ -283,4 +281,7 @@ function defineImagesLoaded( EventEmitter, eventie ) {
 
   return ImagesLoaded;
 
-}}));
+}));
+
+
+
